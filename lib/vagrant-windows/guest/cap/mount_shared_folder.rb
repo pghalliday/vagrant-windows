7cbadeef694a38bf7d790dd1fee0bb4a927b89e7
Peter Halliday <pghalliday@gmail.com>
2013-11-14 16:42:41 +0000

    added drive_letter option for synced_folder but doesn't help with chef issue

--- lib/vagrant-windows/guest/cap/mount_shared_folder.rb
+++ lib/vagrant-windows/guest/cap/mount_shared_folder.rb
@@ -6,22 +6,24 @@ module VagrantWindows
       class MountSharedFolder
         
         def self.mount_virtualbox_shared_folder(machine, name, guestpath, options)
-          mount_shared_folder(machine, name, guestpath, "\\\\vboxsrv\\")
+          mount_shared_folder(machine, name, guestpath, options, "\\\\vboxsrv\\")
         end
         
         def self.mount_vmware_shared_folder(machine, name, guestpath, options)
-          mount_shared_folder(machine, name, guestpath, "\\\\vmware-host\\Shared Folders\\")
+          mount_shared_folder(machine, name, guestpath, options, "\\\\vmware-host\\Shared Folders\\")
         end
         
         protected
         
-        def self.mount_shared_folder(machine, name, guestpath, vm_provider_unc_base)
+        def self.mount_shared_folder(machine, name, guestpath, options, vm_provider_unc_base)
           share_name = VagrantWindows::Helper.win_friendly_share_id(name)
-          options = {
+          drive_letter = options.fetch(:drive_letter, nil)
+          template_options = {
             :mount_point => guestpath,
             :share_name => share_name,
-            :vm_provider_unc_path => vm_provider_unc_base + share_name}
-          mount_script = VagrantWindows.load_script_template("mount_volume.ps1", :options => options)
+            :vm_provider_unc_path => vm_provider_unc_base + share_name,
+            :drive_letter => drive_letter}
+          mount_script = VagrantWindows.load_script_template("mount_volume.ps1", :options => template_options)
           machine.communicate.execute(mount_script, {:shell => :powershell})
         end
         
--- spec/vagrant-windows/mount_shared_folder_spec.rb
+++ spec/vagrant-windows/mount_shared_folder_spec.rb
@@ -30,5 +30,27 @@ describe VagrantWindows::Guest::Cap::MountSharedFolder, :unit => true do
         @machine, "vagrant", "/tmp/vagrant", {})
     end
   end
-  
+ 
+  describe "mount_virtualbox_shared_folder_with_drive_letter" do
+    it "should assign drive letter persistently in call to net use"  do
+      @communicator.expects(:execute).with do |script, options|
+        expect(script).to include("& net use X: $VmProviderUncPath /persistent:yes 2>&1 | out-null")
+      end      
+
+      VagrantWindows::Guest::Cap::MountSharedFolder.mount_virtualbox_shared_folder(
+        @machine, "vagrant", "/tmp/vagrant", {:drive_letter => "X"})
+    end
+  end
+ 
+  describe "mount_vmware_shared_folder_with_drive_letter" do
+    it "should assign drive letter persistently in call to net use"  do
+      @communicator.expects(:execute).with do |script, options|
+        expect(script).to include("& net use X: $VmProviderUncPath /persistent:yes 2>&1 | out-null")
+      end
+      
+      VagrantWindows::Guest::Cap::MountSharedFolder.mount_vmware_shared_folder(
+        @machine, "vagrant", "/tmp/vagrant", {:drive_letter => "X"})
+    end
+  end
+
 end
